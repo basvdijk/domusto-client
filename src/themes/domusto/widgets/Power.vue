ac53b0ce26833a1bc6926cf186ec40319d3f7624
Bas van Dijk <bas@usto.nl>
2017-09-14 20:56:29 +0200

    Implemented delivered / received for P1 widget

--- src/themes/domusto/widgets/Power.vue
+++ src/themes/domusto/widgets/Power.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="widget widget--input">
-    <div class="widget__label">
+    <div class="widget__label" v-if="sensor.protocol.type == 'received'">
       
         {{ round(sensor.data.electricity.received.actual.value, 3) }}
         {{ sensor.data.electricity.received.actual.unit }}
@@ -14,6 +14,24 @@
         
           {{ round(sensor.data.electricity.received.tariff2.value, 0) }}
           {{ sensor.data.electricity.received.tariff2.unit }}
+
+        </div>
+      
+    </div>
+    <div class="widget__label" v-if="sensor.protocol.type == 'delivered'">
+      
+        {{ round(sensor.data.electricity.delivered.actual.value, 3) }}
+        {{ sensor.data.electricity.delivered.actual.unit }}
+      
+        <div>
+      
+          {{ round(sensor.data.electricity.delivered.tariff1.value, 0) }}
+          {{ sensor.data.electricity.delivered.tariff1.unit }}
+        
+          /
+        
+          {{ round(sensor.data.electricity.delivered.tariff2.value, 0) }}
+          {{ sensor.data.electricity.delivered.tariff2.unit }}
 
         </div>
       
