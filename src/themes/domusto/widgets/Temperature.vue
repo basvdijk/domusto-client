b230fffab224fad0ba0b7f695ae9486bc37809da
Bas van Dijk <bas@usto.nl>
2017-09-16 14:42:48 +0200

    Made humidity / temp field only show when data is available

--- src/themes/domusto/widgets/Temperature.vue
+++ src/themes/domusto/widgets/Temperature.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="widget widget--input">
     <div class="widget__label">
-      <div class="widget__label--temperature">{{ round(sensor.data.temperature, 1) }} &deg;C</div>
+      <div v-if="sensor.data.temperature" class="widget__label--temperature">{{ round(sensor.data.temperature, 1) }} &deg;C</div>
   
-      <div class="widget__label widget__label--humidity">{{ sensor.data.humidity }}%</div>
+      <div v-if="sensor.data.humidity" class="widget__label widget__label--humidity">{{ sensor.data.humidity }}%</div>
   
       <div class="widget__status-indicator" v-bind:class="{ 'widget__status-indicator--red': (sensor.data.batteryLevel === 0) }"></div>
       <div class="widget__title">{{ sensor.name }}</div>
